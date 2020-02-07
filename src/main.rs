f3d4238ae7a9be558ac8a6a3c879a2d0c892012d
Armando Perez <gmandx@gmail.com>
2020-02-07 10:25:47 -0800

    Using itertools to merge collections of documents (like YAML allows)

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ use std::path::PathBuf;
 
 use structopt::StructOpt;
 // use toml;
+use itertools::{EitherOrBoth, Itertools};
 use json;
 use yaml_rust as yaml;
+
+use conversions::{JsonValue, YamlValue};
+use merging::{ArrayMergeBehavior, DeepMerge};
 
 #[derive(Debug)]
 pub(crate) enum Document {
@@ -20,13 +24,75 @@ pub(crate) enum Document {
 
 #[derive(Debug)]
 pub(crate) enum DocumentError {
-    Skipped { filename: PathBuf },
-    Loading { filename: PathBuf, error: Box<dyn Error> },
+    Skipped {
+        filename: PathBuf,
+    },
+    Loading {
+        filename: PathBuf,
+        error: Box<dyn Error>,
+    },
 }
 
 impl Document {
-    pub fn merge(self, with: Self) -> Self {
-        todo!()
+    pub fn deep_merge(self, with: Self, array_merge: ArrayMergeBehavior) -> Self {
+        match (self, with) {
+            (Self::YAML(left), Self::YAML(right)) => Self::YAML(
+                left.into_iter()
+                    .zip_longest(right.into_iter())
+                    .map(|zipped| match zipped {
+                        EitherOrBoth::Both(left, right) => {
+                            YamlValue(left).deep_merge(YamlValue(right), array_merge).0
+                        }
+                        EitherOrBoth::Left(left) => left,
+                        EitherOrBoth::Right(right) => right,
+                    })
+                    .collect(),
+            ),
+            (Self::JSON(left), Self::JSON(right)) => Self::JSON(
+                left.into_iter()
+                    .zip_longest(right.into_iter())
+                    .map(|zipped| match zipped {
+                        EitherOrBoth::Both(left, right) => {
+                            JsonValue(left).deep_merge(JsonValue(right), array_merge).0
+                        }
+                        EitherOrBoth::Left(left) => left,
+                        EitherOrBoth::Right(right) => right,
+                    })
+                    .collect(),
+            ),
+            (Self::JSON(left), Self::YAML(right)) => Self::JSON(
+                left.into_iter()
+                    .zip_longest(
+                        right
+                            .into_iter()
+                            .map(|yml| JsonValue::from(YamlValue(yml)).0),
+                    )
+                    .map(|zipped| match zipped {
+                        EitherOrBoth::Both(left, right) => {
+                            JsonValue(left).deep_merge(JsonValue(right), array_merge).0
+                        }
+                        EitherOrBoth::Left(left) => left,
+                        EitherOrBoth::Right(right) => right,
+                    })
+                    .collect(),
+            ),
+            (Self::YAML(left), Self::JSON(right)) => Self::YAML(
+                left.into_iter()
+                    .zip_longest(
+                        right
+                            .into_iter()
+                            .map(|yml| YamlValue::from(JsonValue(yml)).0),
+                    )
+                    .map(|zipped| match zipped {
+                        EitherOrBoth::Both(left, right) => {
+                            YamlValue(left).deep_merge(YamlValue(right), array_merge).0
+                        }
+                        EitherOrBoth::Left(left) => left,
+                        EitherOrBoth::Right(right) => right,
+                    })
+                    .collect(),
+            ),
+        }
     }
 }
 
@@ -37,45 +103,59 @@ struct CliArgs {
     /// Files to process
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
+
+    #[structopt(long = "arrays")]
+    array_merge: ArrayMergeBehavior,
 }
 
 fn main() {
-    let cli_args = CliArgs::from_args();
+    let CliArgs {
+        files: filenames,
+        array_merge,
+    } = CliArgs::from_args();
 
-    let mut documents = cli_args
-        .files
+    let mut documents = filenames
         .into_iter()
         .filter_map(|filename| {
             let contents = match read_to_string(&filename) {
                 Ok(contents) => contents,
-                Err(error) => return Some(Err(DocumentError::Loading { filename, error: Box::new(error) })),
+                Err(error) => {
+                    return Some(Err(DocumentError::Loading {
+                        filename,
+                        error: Box::new(error),
+                    }))
+                }
             };
 
             match filename.extension().and_then(OsStr::to_str) {
                 Some("yml") | Some("yaml") => match yaml::YamlLoader::load_from_str(&contents) {
                     Ok(loaded) => Some(Ok(Document::YAML(loaded))),
-                    Err(error) => Some(Err(DocumentError::Loading { filename, error: Box::new(error) })),
+                    Err(error) => Some(Err(DocumentError::Loading {
+                        filename,
+                        error: Box::new(error),
+                    })),
                 },
                 Some("json") => match json::parse(&contents) {
                     Ok(loaded) => Some(Ok(Document::JSON(vec![loaded]))),
-                    Err(error) => Some(Err(DocumentError::Loading { filename, error: Box::new(error) })),
+                    Err(error) => Some(Err(DocumentError::Loading {
+                        filename,
+                        error: Box::new(error),
+                    })),
                 },
                 Some(_) => Some(Err(DocumentError::Skipped { filename })),
                 None => None,
             }
         })
-        .filter(|loaded| {
-            match loaded {
-                Err(DocumentError::Skipped { filename }) => {
-                    eprintln!("Skipped {:?}", filename);
-                    false
-                },
-                Err(DocumentError::Loading { filename, error }) => {
-                    eprintln!("Error loading {:?}: {:?}", filename, error);
-                    false
-                }
-                Ok(_) => true,
+        .filter_map(|loaded| match loaded {
+            Err(DocumentError::Skipped { filename }) => {
+                eprintln!("Skipped {:?}", filename);
+                None
             }
+            Err(DocumentError::Loading { filename, error }) => {
+                eprintln!("Error loading {:?}: {:?}", filename, error);
+                None
+            }
+            Ok(document) => Some(document),
         });
 
     let destination = match documents.next() {
@@ -85,4 +165,27 @@ fn main() {
             std::process::exit(1);
         }
     };
+
+    let result = documents.fold(destination, |destination, document| {
+        destination.deep_merge(document, array_merge)
+    });
+
+    println!(
+        "{}",
+        match result {
+            Document::JSON(json) => json
+                .into_iter()
+                .map(|doc| doc.pretty(2))
+                .collect::<String>(),
+            Document::YAML(yaml) => yaml
+                .into_iter()
+                .map(|doc| {
+                    let mut out_str = String::new();
+                    let mut emitter = yaml::YamlEmitter::new(&mut out_str);
+                    emitter.dump(&doc).unwrap();
+                    out_str
+                })
+                .collect(),
+        }
+    );
 }
--- src/merging/mod.rs
+++ src/merging/mod.rs
@@ -1,6 +1,8 @@
 mod json;
 mod yaml;
 
+use std::fmt;
+
 #[derive(Debug, Copy, Clone)]
 pub(crate) enum ArrayMergeBehavior {
     Replace,
@@ -11,6 +13,33 @@ pub(crate) enum ArrayMergeBehavior {
     //       For arrays of unequal lengths, simply use the value
     //       that's already there.
     // Zip,
+}
+
+impl Default for ArrayMergeBehavior {
+    fn default() -> Self {
+        ArrayMergeBehavior::Replace
+    }
+}
+
+impl std::str::FromStr for ArrayMergeBehavior {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<ArrayMergeBehavior, &'static str> {
+        match s {
+            "Replace" | "replace" => Ok(Self::Replace),
+            "Concat" | "concat" => Ok(Self::Concat),
+            _ => Err("Not `replace` | `concat`"),
+        }
+    }
+}
+
+impl fmt::Display for ArrayMergeBehavior {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Replace => write!(f, "replace"),
+            Self::Concat => write!(f, "concat"),
+        }
+    }
 }
 
 // TODO: Find better names for `current` and `next` variables/symbols.
